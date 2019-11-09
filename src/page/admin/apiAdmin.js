fca098b80e86d15f572501545c3f5f5a1626a97d
maxzhang686 <maxzhang686@gmail.com>
2019-11-09 15:15:25 +1100

    Add 4 product api, get put....

--- src/page/admin/apiAdmin.js
+++ src/page/admin/apiAdmin.js
@@ -85,3 +85,69 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
     })
     .catch(err => console.log(err));
 };
+
+/**
+ * to perform crud on product
+ * get all products
+ * get a single product
+ * update single product
+ * delete single product
+ */
+
+export const getProducts = () => {
+  return fetch(`${API}/products?limit=undefined`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const getSingleProduct = productId => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const deleteProduct = (productId, userId, token) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+/**
+ * to perform crud on category
+ * get all category //alread exist
+ * get a single category
+ * update single category
+ * delete single category
+ */
